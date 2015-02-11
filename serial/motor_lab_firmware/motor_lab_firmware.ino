1fd24a781082b012e96f733983470f066bda6fbd
Ian Hartwig <spacepenguine@gmail.com>
2015-02-11 03:58:54 -0500

    restructure main loop, hook up mode switch, and set up velocity PID

--- serial/motor_lab_firmware/motor_lab_firmware.ino
+++ serial/motor_lab_firmware/motor_lab_firmware.ino
@@ -1,33 +1,31 @@
 #include <PID_v1.h>
+
 #include <Encoder.h>
+
 #include <Servo.h>
 
 // hardware configuration
 #define INPUT_POT A0
-#define INPUT_BEND A4
+#define INPUT_RANGE A4
+#define INPUT_BEND A1
 #define INPUT_LIMIT 4
 #define OUTPUT_DEBUG 13
-#define OUTPUT_SERVO 5
-#define OUTPUT_MOTOR_FORWARD 12
-#define OUTPUT_MOTOR_BACKWARD 13
+#define OUTPUT_SERVO 9
+#define OUTPUT_MOTOR_FORWARD 5
+#define OUTPUT_MOTOR_BACKWARD 6
 #define OUTPUT_STEPPER_STEP 7
 #define OUTPUT_STEPPER_DIR 8
-#define OUTPUT_STEPPER_EN 9
+#define OUTPUT_STEPPER_EN 13
 #define INPUT_ENCODER_A 2  //should be on an interrupt pin 
 #define INPUT_ENCODER_B 3  //should be on an interrupt pin 
-#define KSPP 1
-#define KSPI 0
-#define KSPD 0 
-#define KSVP 1
-#define KSVI 0
-#define KSVD 0 
-#define KSPP 1
+#define KDPP 1
 #define KDPI 0
 #define KDPD 0
-#define KSVP 1
+#define KDVP 1
 #define KDVI 0
 #define KDVD 0
 
+#define ENCODER_DT_MIN_MS 50
 #define STEPPER_DELAY_US 2000
 #define FILTER_SIZE 32
 
@@ -60,46 +58,50 @@ unsigned int filter_add(struct FilterData *filter_data, unsigned int new_value) 
   return (filter_data->total)/FILTER_SIZE;
 }
 
-
 // global data
-unsigned int PotVal=0;
-unsigned int BendVal=0;
-unsigned int LimitVal=0;
-unsigned int stepper_position = 0; //TODO should this be an unsigned int 
-int stepper_velocity = 0;
+// sensor readings
+unsigned int encoder_position = 0;
+unsigned int encoder_position_old = 0;
+double encoder_velocity = 0;
+unsigned int pot_value = 0;
+unsigned int range_value = 0;
+unsigned int bend_value = 0;
+uint8_t limit_value_old = 0;
+uint8_t limit_value = 0;
+// setpoints
+unsigned int stepper_position = 0; //TODO should this be an unsigned int
+unsigned int stepper_setpoint = 0;
+uint8_t servo_setpoint = 0;
+uint8_t motor_pwm_setpoint = 0;
+uint8_t motor_pwm_direction = 0;
+// program mode 0 = sensor, 1 = gui, position, 2 = gui, velocity
+uint8_t program_mode = 0;
+// other
 unsigned int data_timer = 0;
-double DC_position = 0;
-double old_DC_position = 0;
-double DC_velocity = 0;
-int DC_Speed = 0;    //Using fixed point for reduced space usage 
-int incomingByte = 0; //for incoming serial data
+uint8_t incomingByte = 0; // for incoming serial data
+unsigned long time_old = 0;
+unsigned long time_now = 0;
+unsigned long time_dt = 0; // time between encoder updates
+// PID
+double pid_position_input;
+double pic_position_output;
+double pid_position_setpoint;
+double pid_velocity_input;
+double pid_velocity_output;
+double pid_velocity_setpoint;
 
-//PID
-double Input_stepper_position_PID;
-double Input_stepper_velocity_PID;
-double Input_dc_position_PID;
-double Input_dc_velocity_PID;
-
-double Output_stepper_position_PID;
-double Output_stepper_velocity_PID;
-double Output_dc_position_PID;
-double Output_dc_velocity_PID;
-
-double Setpoint_stepper_position_PID;
-double Setpoint_stepper_velocity_PID;
-double Setpoint_dc_position_PID;
-double Setpoint_dc_velocity_PID;
-
-//time between encoder updates 
-double dt;
-
-PID stepper_position_PID(&Input_stepper_position_PID,&Output_stepper_position_PID,&Setpoint_stepper_position_PID,KSPP,KSPI,KSPD,DIRECT);
-PID stepper_velocity_PID(&Input_stepper_velocity_PID,&Output_stepper_velocity_PID,&Setpoint_stepper_velocity_PID,KSVP,KSVI,KSVD,DIRECT);
-PID dc_position_PID(&Input_dc_position_PID,&Output_stepper_position_PID,&Setpoint_stepper_position_PID,KSPP,KDPI,KDPD,DIRECT);
-PID dc_velocity_PID(&Input_dc_velocity_PID,&Output_stepper_velocity_PID,&Setpoint_stepper_velocity_PID,KSVP,KDVI,KDVD,DIRECT);
+PID dc_position_PID(&pid_position_input,
+                    &pic_position_output,
+                    &pid_position_setpoint,
+                    KDPP,KDPI,KDPD,DIRECT);
+PID dc_velocity_PID(&pid_velocity_input,
+                    &pid_velocity_output,
+                    &pid_velocity_setpoint,
+                    KDVP,KDVI,KDVD,DIRECT);
 Servo servo;
 Encoder kencoder(INPUT_ENCODER_A,INPUT_ENCODER_B);
 struct FilterData bend_filter;
+struct FilterData range_filter;
 
 
 
@@ -109,6 +111,7 @@ void setup() {
   // establish direction of pins we are using to drive LEDs 
   pinMode(INPUT_POT, INPUT); 
   //pinMode(BendBefore, OUTPUT); 
+  pinMode(INPUT_RANGE, INPUT);
   pinMode(INPUT_BEND, INPUT);
   pinMode(INPUT_LIMIT, INPUT_PULLUP);
   pinMode(OUTPUT_DEBUG, OUTPUT);
@@ -128,11 +131,7 @@ void setup() {
   
   filter_init(&bend_filter);
 
-/*  Input = DC_position;
-  Setpoint = INPUT_POT;
-  myPID.SetMode(AUTOMATIC);
-*/
-  
+  dc_velocity_PID.SetMode(AUTOMATIC);
   
   Serial.begin(9600);
 }
@@ -160,44 +159,108 @@ void step() {
 }
 
 void output_serial_data() {
-  Serial.print(PotVal);
+  Serial.print(pot_value);
   Serial.print(" ");
-  Serial.print(BendVal);
+  Serial.print(range_value);
   Serial.print(" ");
-  Serial.print(LimitVal);
-  long newEncoder = kencoder.read();
+  Serial.print(bend_value);
   Serial.print(" ");
-  Serial.print(newEncoder);
+  Serial.print(encoder_position);
   Serial.print(" ");
-  Serial.print(incomingByte);
+  Serial.print(encoder_velocity);
+  Serial.print(" ");
+  Serial.print(time_dt);
+  Serial.print(" ");
+  Serial.print(pid_velocity_input);
+  Serial.print(" ");
+  Serial.print(pid_velocity_output);
+  Serial.print(" ");
+  Serial.print(pid_velocity_setpoint);
+  Serial.print(" ");
+  Serial.print(program_mode);
   Serial.println();
 }
 
-void noGUI(){
+
+/* Main routine (called repeated by from the Arduino framework) */
+void loop() {
+  data_timer++;
   
-  // set servo position
-  // use the limit switch to move between 2 set positions
-  if (LimitVal == 1) {
-    servo.write(45);
-  } else {
-    servo.write(135);
+  // read sensor data
+  if(millis() - time_now > ENCODER_DT_MIN_MS) {
+    // time
+    time_old = time_now;
+    time_now = millis();
+    time_dt = time_now - time_old;
+    // encoder update on slow cycle
+    encoder_position_old = encoder_position;
+    encoder_position = kencoder.read();
+    encoder_velocity = ((double)(encoder_position - encoder_position_old))/((double)time_dt);
   }
-//  analogWrite(OUTPUT_MOTOR_FORWARD,Output);
-      
+  // analog sensors
+  pot_value = analogRead(INPUT_POT);
+  range_value = filter_add(&range_filter, analogRead(INPUT_RANGE));
+  bend_value = filter_add(&bend_filter, analogRead(INPUT_BEND));
+  limit_value_old = limit_value;
+  limit_value = digitalRead(INPUT_LIMIT);
+
+  //checks to see if data has been sent 
+  if (Serial.available() > 0){
+       //read the incoming byte:
+      incomingByte = Serial.read();
+  }
+  
+  // change program mode, if necessary
+  if(limit_value_old == 0 && limit_value == 1) {
+    if(program_mode < 3) {
+      program_mode++;
+    } else {
+      program_mode = 0;
+    }
+  }
+  
+  
+  // determine proper control settings
+  if(program_mode == 1) {
+    // gui control position
+    // todo
+  } else if (program_mode == 2) {
+    // gui control velocity
+    // todo
+  } else if (program_mode == 3) {
+    // sensor control with motor position
+    
+    
+  } else {
+    stepper_setpoint = range_value; // 0-1024
+    
+    // user motor velocity pid
+    pid_velocity_input = encoder_velocity * 512;
+    pid_velocity_setpoint = (double)(pot_value/4);
+    dc_velocity_PID.Compute();
+    motor_pwm_setpoint = (uint8_t)pid_velocity_output;
+    // servo_setpoint = bend_value;
+    servo_setpoint = 45;
+  }
+  
+  // output settings
+  // set servo
+  servo.write(servo_setpoint);
+  
   // set motor output based on pot value
-  if (PotVal < 16) {
+  if (motor_pwm_setpoint < 16) {
     // have a dead-band for low values
     analogWrite(OUTPUT_MOTOR_FORWARD, 0);
-//    analogWrite(OUTPUT_MOTOR_BACKWARD, 0);
+    analogWrite(OUTPUT_MOTOR_BACKWARD, 0);
   } else {
     // rescale output since analog values are 0-1023
-    analogWrite(OUTPUT_MOTOR_FORWARD, PotVal/4);
-//    analogWrite(OUTPUT_MOTOR_BACKWARD, 0);
+    analogWrite(OUTPUT_MOTOR_FORWARD, motor_pwm_setpoint);
+    analogWrite(OUTPUT_MOTOR_BACKWARD, 0);
   }
-
+  
   
   // set stepper motor to position based on bend sensor
-  int stepper_position_error = BendVal - stepper_position;
+  int stepper_position_error = stepper_setpoint - stepper_position;
   if (stepper_position_error > 8) {
     // set direction
     digitalWrite(OUTPUT_STEPPER_DIR, HIGH);
@@ -208,132 +271,11 @@ void noGUI(){
     step();
     stepper_position--;
   }
-}
-
-
-/* Main routine (called repeated by from the Arduino framework) */
-void loop() {
-  dt = 1; //TODO change this 
-  old_DC_position = DC_position;
-  DC_position = kencoder.read();
-  DC_velocity = (DC_position - old_DC_position)/dt;
-  PotVal = analogRead(INPUT_POT);
-  BendVal = filter_add(&bend_filter, analogRead(INPUT_BEND));
-  LimitVal = digitalRead(INPUT_LIMIT);
-
-  data_timer++;
-  if (data_timer > 50) {
+  
+  // output relevant data
+  if (data_timer == 50) {
     data_timer = 0;
     output_serial_data();
   }
-
-
-  //checks to see if data has been sent 
-  if (Serial.available() > 0){
-       //read the incoming byte:
-      incomingByte = Serial.read();
-  }
-  
-  //8 bits
-
-// incomingByte = 0 then use all sensors on the board
-// incomingByte > 0 then use gui 
-// incomingByte_firstbit = 0 then set position
-// incomingByte_firstbit = 1 then set velocity
-// incomingByte_secondbit = 0 then servo = low extreme  
-// incomingByte_secondbit = 1 then servo = high extreme
-// incomingByte_thridBit = 0 then modify steper motor
-// incomingByte_thridBit = 1 then modify DC motor
-// incomingByte_fourthbit = direction
-// incomingByte_5 - 8 = 0001 = 0
-// incomingByte_5 - 8 = 0001 = 1
-// incomingByte_5 - 8 = 0010 = 2
-// incomingByte_5 - 8 = 0011 = 3
-// incomingByte_5 - 8 = 0100 = 4
-// incomingByte_5 - 8 = 0101 = 5
-// incomingByte_5 - 8 = 0110 = 6
-// incomingByte_5 - 8 = 0111 = 7
-// incomingByte_5 - 8 = 1000 = 8
-// incomingByte_5 - 8 = 1001 = 9
-// incomingByte_5 - 8 = 1010 = 10
-// incomingByte_5 - 8 = 1011 = 11
-// incomingByte_5 - 8 = 1100 = 12
-// incomingByte_5 - 8 = 1101 = 13
-// incomingByte_5 - 8 = 1110 = 14
-// incomingByte_5 - 8 = 1111 = 15
-if(incomingByte == 0){
-  noGUI();
-}else{
- boolean velocityMode = ((incomingByte >> 7) & B10000000) == 1;
- boolean servoHigh    = ((incomingByte >> 6) & B01000000) == 1; 
- boolean stepperMotor  = ((incomingByte >> 5) & B00100000) == 1;
- boolean dir          = ((incomingByte >> 4) & B00010000) == 1;
- unsigned int speed_  = (incomingByte        & B00001111);
- //puts sign on the speed of the motor
- int delta = speed_;
- if(dir == 1){
-    delta = -delta;
- } 
- 
- if(servoHigh){
-   //set servo   
-   servo.write(180);
- }else{
-   //set servo low 
-   servo.write(45);
- }
- 
- double Input_stepper_position_PID;
-double Input_stepper_velocity_PID;
-double Input_dc_position_PID;
-double Input_dc_velocity_PID;
-
-double Output_stepper_position_PID;
-double Output_stepper_velocity_PID;
-double Output_dc_position_PID;
-double Output_dc_velocity_PID;
-
-double Setpoint_stepper_position_PID;
-double Setpoint_stepper_velocity_PID;
-double Setpoint_dc_position_PID;
-double Setpoint_dc_velocity_PID;
- 
-  if(stepperMotor){
-    if(velocityMode){
-     //set steperMotor velocityMode
-     Input_stepper_velocity_PID =  stepper_velocity;
-     //TODO update setpoint
-     stepper_velocity_PID.Compute();
-     //todo send value  
-     }else{
-      //set steperMotor Position Mode     
-      Input_stepper_position_PID = stepper_position;
-      //TODO update setpoint
-      stepper_position_PID.Compute();
-      //TODO send value 
-    }
-  }else{
-    if(velocityMode){
-     //set DCMotor velocityMode
-     Input_dc_velocity_PID = DC_velocity;
-     //TODO update setpoint
-     dc_velocity_PID.Compute();
-     //TDOO send value 
-    }else{
-      //set DCMotor Position Mode
-      Input_dc_position_PID = DC_position;
-      //TODO update setpoint
-      dc_position_PID.Compute();
-      //TODO send value 
-      
-    }
-  }
- 
 }
-  
 
-
-//delay(1);
-//step();
-
-}  // end loop()
