0d58ed606140e835f2494d7b6f1c8b76962b6fe9
Ian Hartwig <spacepenguine@gmail.com>
2015-02-19 21:15:02 -0500

    remove actual work from the website branch

--- serial/testSerial.py
+++
@@ -1,8 +0,0 @@
-# Written by Trevor Decker 
-# reads 100 bytes from a serial port and displays them to the user 
-
-import serial
-ser = serial.Serial('/dev/ttyACM0')
-line = ser.read(100)
-print line
-ser.close()
