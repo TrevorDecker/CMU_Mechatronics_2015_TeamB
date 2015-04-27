7c904591c7694103b76d8717d0c80414503e7bb1
Ian Hartwig <spacepenguine@gmail.com>
2015-04-27 01:56:41 -0400

    eliminate HAL_ADC_MspInit silliness. we can setup everything in our init function first.

--- stm32f4-firmware/Src/main.c
+++ stm32f4-firmware/Src/main.c
@@ -22,6 +22,7 @@ volatile int adc_conv_count = 0;
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void Error_Handler(void);
+void HAL_ADC_MspInit2(ADC_HandleTypeDef* hadc);
 
 /* Private functions ---------------------------------------------------------*/
 
@@ -193,6 +194,41 @@ int main(void)
   AdcHandle.Init.NbrOfConversion = 2;
   AdcHandle.Init.DMAContinuousRequests = ENABLE;
   AdcHandle.Init.EOCSelection = DISABLE;
+
+  /* ADC3 Periph clock enable */
+  ADCx_CLK_ENABLE();
+  /* Enable DMA2 clock */
+  DMAx_CLK_ENABLE(); 
+  
+  /*##-2- Configure peripheral GPIO ##########################################*/ 
+  /* ADC3 Channel8 GPIO pin configuration */
+  // GPIO_InitStruct.Pin = ADCx_CHANNEL_PIN;
+  // GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  // GPIO_InitStruct.Pull = GPIO_NOPULL;
+  // HAL_GPIO_Init(ADCx_CHANNEL_GPIO_PORT, &GPIO_InitStruct);
+  
+  /*##-3- Configure the DMA streams ##########################################*/
+  /* Set the parameters to be configured */
+  static DMA_HandleTypeDef  hdma_adc;
+  hdma_adc.Instance = ADCx_DMA_STREAM;
+  
+  hdma_adc.Init.Channel  = ADCx_DMA_CHANNEL;
+  hdma_adc.Init.Direction = DMA_PERIPH_TO_MEMORY;
+  hdma_adc.Init.PeriphInc = DMA_PINC_DISABLE;
+  hdma_adc.Init.MemInc = DMA_MINC_ENABLE;
+  hdma_adc.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+  hdma_adc.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+  hdma_adc.Init.Mode = DMA_CIRCULAR;
+  hdma_adc.Init.Priority = DMA_PRIORITY_HIGH;
+  hdma_adc.Init.FIFOMode = DMA_FIFOMODE_DISABLE;         
+  hdma_adc.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_HALFFULL;
+  hdma_adc.Init.MemBurst = DMA_MBURST_SINGLE;
+  hdma_adc.Init.PeriphBurst = DMA_PBURST_SINGLE; 
+
+  HAL_DMA_Init(&hdma_adc);
+    
+  /* Associate the initialized DMA handle to the the ADC handle */
+  AdcHandle.DMA_Handle = &hdma_adc;
       
   if(HAL_ADC_Init(&AdcHandle) != HAL_OK)
   {
@@ -367,108 +403,6 @@ static void SystemClock_Config(void)
     /* Initialization Error */
     Error_Handler();
   }
-}
-
-
-/**
-  * @brief ADC MSP Initialization 
-  *        This function configures the hardware resources used in this example: 
-  *           - Peripheral's clock enable
-  *           - Peripheral's GPIO Configuration  
-  * @param huart: UART handle pointer
-  * @retval None
-  */
-void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
-{
-  // GPIO_InitTypeDef          GPIO_InitStruct;
-  static DMA_HandleTypeDef  hdma_adc;
-  
-  /*##-1- Enable peripherals and GPIO Clocks #################################*/
-  /* Enable GPIO clock */
-  // ADCx_CHANNEL_GPIO_CLK_ENABLE();
-  /* ADC3 Periph clock enable */
-  ADCx_CLK_ENABLE();
-  /* Enable DMA2 clock */
-  DMAx_CLK_ENABLE(); 
-  
-  /*##-2- Configure peripheral GPIO ##########################################*/ 
-  /* ADC3 Channel8 GPIO pin configuration */
-  // GPIO_InitStruct.Pin = ADCx_CHANNEL_PIN;
-  // GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  // GPIO_InitStruct.Pull = GPIO_NOPULL;
-  // HAL_GPIO_Init(ADCx_CHANNEL_GPIO_PORT, &GPIO_InitStruct);
-  
-  /*##-3- Configure the DMA streams ##########################################*/
-  /* Set the parameters to be configured */
-  hdma_adc.Instance = ADCx_DMA_STREAM;
-  
-  hdma_adc.Init.Channel  = ADCx_DMA_CHANNEL;
-  hdma_adc.Init.Direction = DMA_PERIPH_TO_MEMORY;
-  hdma_adc.Init.PeriphInc = DMA_PINC_DISABLE;
-  hdma_adc.Init.MemInc = DMA_MINC_ENABLE;
-  hdma_adc.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
-  hdma_adc.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
-  hdma_adc.Init.Mode = DMA_CIRCULAR;
-  hdma_adc.Init.Priority = DMA_PRIORITY_HIGH;
-  hdma_adc.Init.FIFOMode = DMA_FIFOMODE_DISABLE;         
-  hdma_adc.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_HALFFULL;
-  hdma_adc.Init.MemBurst = DMA_MBURST_SINGLE;
-  hdma_adc.Init.PeriphBurst = DMA_PBURST_SINGLE; 
-
-  HAL_DMA_Init(&hdma_adc);
-    
-  /* Associate the initialized DMA handle to the the ADC handle */
-  __HAL_LINKDMA(hadc, DMA_Handle, hdma_adc);
-
-  /*##-4- Configure the NVIC for DMA #########################################*/
-  /* NVIC configuration for DMA transfer complete interrupt */
-  // HAL_NVIC_SetPriority(ADCx_DMA_IRQn, 0, 0);   
-  // HAL_NVIC_EnableIRQ(ADCx_DMA_IRQn);
-}
-
-
-/**
-  * @brief ADC MSP De-Initialization 
-  *        This function frees the hardware resources used in this example:
-  *          - Disable the Peripheral's clock
-  *          - Revert GPIO to their default state
-  * @param hadc: ADC handle pointer
-  * @retval None
-  */
-void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
-{
-  static DMA_HandleTypeDef  hdma_adc;
-  
-  /*##-1- Reset peripherals ##################################################*/
-  __ADC_FORCE_RESET();
-  __ADC_RELEASE_RESET();
-
-  /*##-2- Disable peripherals and GPIO Clocks ################################*/
-  /* De-initialize the ADC3 Channel8 GPIO pin */
-  // HAL_GPIO_DeInit(ADCx_CHANNEL_GPIO_PORT, ADCx_CHANNEL_PIN);
-  
-  /*##-3- Disable the DMA Streams ############################################*/
-  /* De-Initialize the DMA Stream associate to transmission process */
-  HAL_DMA_DeInit(&hdma_adc); 
-    
-  /*##-4- Disable the NVIC for DMA ###########################################*/
-  // HAL_NVIC_DisableIRQ(ADCx_DMA_IRQn);
-}
-
-
-/**
-  * @brief  Conversion complete callback in non blocking mode 
-  * @param  AdcHandle : AdcHandle handle
-  * @note   This example shows a simple way to report end of conversion, and 
-  *         you can add your own implementation.    
-  * @retval None
-  */
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle)
-{
-  /* Turn LED1 on: Transfer process is correct */
-  // HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_SET);
-  // HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_RESET);
-  adc_conv_count++;
 }
 
 
